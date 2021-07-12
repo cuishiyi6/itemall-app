b6c65ba1302a5e303595d975faaf25459ad3b980
cuishiyi6 <1960393658@qq.com>
2021-07-12 14:44:07 +0800

    2021/07/12

---
+++ src/api/home.js
@@ -0,0 +1,18 @@
+/**
+ * 发送首页请求
+ */
+import $http from './request'
+
+// 请求首页数据
+export const queryHome = () => $http({ url: '/home' })
+
+
+// 请求商品数据
+export const queryGoods = (params) => $http({
+    url: '/home/goods', params
+})
+
+// 请求商品详情
+export const queryDetail = (params) => $http({
+    url: '/home/detail', params
+})
