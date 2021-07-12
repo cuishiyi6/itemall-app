2643ee8a023d97a9a3f24d57559c310659b31a48
cuishiyi6 <1960393658@qq.com>
2021-07-12 14:46:07 +0800

    2021/07/12

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
+    <tab-bar/>
   </div>
 </template>
 
+<script>
+import TabBar from './components/tabbar/TabBar.vue'
+export default {
+  components: { TabBar },
+};
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url("//at.alicdn.com/t/font_1431323_0md7akg91rp.css");
+@import url("https://cdn.bootcdn.net/ajax/libs/reseter.css/1.2.0/reseter.min.css");
 </style>
